e38fa28a7cb532b00e12ac280a1d5f8869808326
AndyG <anderson.garron@gmail.com>
2019-02-02 09:19:43 -0800

    all sorts of stuff

---
+++ Assets/Scripts/FSM/FSM.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM2 : MonoBehaviour
+{
+  //Base class for all states, only the required methods need to be overriden
+  public class StateBase
+  {
+    [HideInInspector] public FSM2 fsm;
+
+    public virtual void Update() { }
+    public virtual void Exit() { }
+  };
+
+  public class State : StateBase { public virtual void Enter() { } }
+  public abstract class State1Param<T> : StateBase { public abstract void Enter(T p); }
+  public abstract class State2Params<T0, T1> : StateBase { public abstract void Enter(T0 p0, T1 p1); }
+  public abstract class State3Params<T0, T1, T2> : StateBase { public abstract void Enter(T0 p0, T1 p1, T2 p2); }
+
+  //Current state being handled in this FSM
+  public StateBase currentState { private set; get; }
+
+  bool ChangeStateBase(StateBase _newState)
+  {
+    //Exit the current state
+    if (currentState != null)
+      currentState.Exit();
+
+    //Change to the new state
+    currentState = _newState;
+
+    if (_newState != null)
+    {
+      _newState.fsm = this;
+      return true;
+    }
+    return false;
+  }
+
+  public void ChangeState(State _newState)
+  {
+    if (ChangeStateBase(_newState)) _newState.Enter();
+  }
+
+  public void ChangeState<T>(State1Param<T> _newState, T p)
+  {
+    if (ChangeStateBase(_newState)) _newState.Enter(p);
+  }
+
+  public void ChangeState<T0, T1>(State2Params<T0, T1> _newState, T0 p0, T1 p1)
+  {
+    if (ChangeStateBase(_newState)) _newState.Enter(p0, p1);
+  }
+
+  public void ChangeState<T0, T1, T2>(State3Params<T0, T1, T2> _newState, T0 p0, T1 p1, T2 p2)
+  {
+    if (ChangeStateBase(_newState)) _newState.Enter(p0, p1, p2);
+  }
+}
---
+++ Assets/Scripts/Player/Manfred.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Manfred : MonoBehaviour
+{
+
+  private State state;
+  private Animator animator;
+  private FSM2 fsm;
+
+  [SerializeField]
+  private float parryDuration;
+
+  private float parryTime = 0f;
+
+  private bool ignoreAnimationEventsThisFrame;
+
+  void Start()
+  {
+    this.animator = GetComponent<Animator>();
+    this.state = State.STANDING;
+  }
+
+  void Update()
+  {
+    ignoreAnimationEventsThisFrame = false;
+    switch (state)
+    {
+      case State.STANDING:
+        UpdateStanding();
+        break;
+      case State.PARRY_START:
+        UpdateParryStart();
+        break;
+      case State.PARRY_ACTIVE:
+        UpdateParryActive();
+        break;
+      default:
+        Debug.Log("no handler for state: " + state);
+        break;
+    }
+  }
+
+  public void OnIdle()
+  {
+    SetState(State.STANDING);
+    animator.SetTrigger("Idle");
+  }
+
+  public void OnParryActive()
+  {
+    SetState(State.PARRY_ACTIVE);
+  }
+
+  private void UpdateStanding()
+  {
+    if (Input.GetKeyDown(KeyCode.P))
+    {
+      SetState(State.PARRY_START);
+      animator.SetTrigger("ParryStart");
+      ignoreAnimationEventsThisFrame = true;
+    }
+  }
+
+  private void UpdateParryStart()
+  {
+    parryTime = 0f;
+    //no-op
+  }
+
+  private void UpdateParryActive()
+  {
+    parryTime += Time.deltaTime;
+    if (parryTime >= parryDuration || !Input.GetKey(KeyCode.P))
+    {
+      SetState(State.PARRY_RETRACT);
+      animator.SetTrigger("ParryRetract");
+      return;
+    }
+  }
+
+  private void SetState(State state)
+  {
+    Debug.Log("SetState: " + state);
+    this.state = state;
+  }
+
+  private enum State
+  {
+    STANDING,
+    PARRY_START,
+    PARRY_ACTIVE,
+    PARRY_RETRACT,
+    PARRY_CATCH
+  }
+}
