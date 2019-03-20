1205bd52d726709ca78d7fa5ba47ba75012d4b84
angarron <anderson.garron@gmail.com>
2019-03-19 23:28:09 -0700

    style nit

--- Assets/Sprites/FireballDeflected.cs
+++ Assets/Sprites/FireballDeflected.cs
@@ -15,7 +15,7 @@ public class FireballDeflected : MonoBehaviour, Directable
 
   public void DirectToward(Vector2 direction) {
     Debug.Log("looking toward: " + direction);
-    float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
+    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
     transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
   }
 }
