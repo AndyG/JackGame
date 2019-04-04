68411700a5c9003abe965b4af930e35f54b44fa1
AndyG <anderson.garron@gmail.com>
2019-04-04 14:33:51 -0700

    blood droplet

---
+++ Assets/Scripts/SiphonSystem/SiphonDroplet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class SiphonDroplet : MonoBehaviour
+{
+
+  private Rigidbody2D rb;
+  private Animator animator;
+
+  public void Awake()
+  {
+    this.rb = GetComponent<Rigidbody2D>();
+    this.animator = GetComponent<Animator>();
+  }
+
+  public void OnEnable()
+  {
+    // set animator to show normal droplet
+  }
+
+  public void AttractToward(Vector2 targetPosition)
+  {
+    Vector3 direction = ((Vector3)targetPosition - transform.position).normalized;
+    rb.MovePosition(transform.position + direction * Time.deltaTime);
+  }
+}
