c964862194493075e119a5673ad340d1d618518f
Shuu <40650038+Shuu-37@users.noreply.github.com>
2019-04-04 23:25:09 -0700

    pause menu script
    pausing

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+     public static bool GameIsPaused = false;
+     public GameObject pauseMenuUI;
+ 
+     // Update is called once per frame
+     public void Update()
+     {
+ 
+         if (Input.GetKeyDown(KeyCode.Escape))
+         {
+             if (GameIsPaused) {
+                 Resume();
+             } else {
+                 Pause();
+             }
+         }
+     }
+ 
+    public void Resume()
+     {
+         pauseMenuUI.SetActive(false);
+         TimeManagerSingleton.Instance.SetTimeScale(1f);
+         GameIsPaused = false;
+     }
+     public void Pause()
+     
+     {
+         pauseMenuUI.SetActive(true);
+         TimeManagerSingleton.Instance.SetTimeScale(0f);
+         GameIsPaused = true;
+     }
+ }
